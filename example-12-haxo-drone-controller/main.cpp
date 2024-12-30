2091aed012345901c6953fcf3c3880306c94fb64
Staffan Melin <staffan.melin@oscillator.se>
2024-12-30 16:53:18 +0100

    Adjust example-12

--- example-12-haxo-drone-controller/main.cpp
+++ example-12-haxo-drone-controller/main.cpp
@@ -115,7 +115,7 @@ bool InitSynths()
 	dhaxo_config.controller_targets = 3;
 	dhaxo_config.controller_target[0] = DSynth::DSYNTH_PARAM_TUNE; 
 	dhaxo_config.controller_target[1] = DSynth::DSYNTH_PARAM_FILTER_CUTOFF; 
-	dhaxo_config.controller_target[2] = DSynth::DSYNTH_PARAM_LFO_FREQ;
+	dhaxo_config.controller_target[2] = DSynth::DSYNTH_PARAM_LFO_AMP;
 	dhaxo_config.synth = &dsynthmelody;
 	dhaxo.Init();
 	dhaxo.Set(dhaxo_config);
